527558d5eb9a78f4279af0c55c2beec35120fdc1
Dustin Dowell <dustindowell22@gmail.com>
2016-09-06 18:54:07 -0500

    gulp for node-sass

--- gulpfile.js
+++ gulpfile.js
@@ -12,13 +12,14 @@ gulp.task('sss', () => {
   return gulp.src('src/**/*.sss')
     .pipe(postcss([], { parser: sugarss }))
     .pipe(rename({ extname: '.scss' }))
-    .pipe(gulp.dest('scss/'))
+    .pipe(gulp.dest('scss'))
 })
 
 gulp.task('scss', ['sss'], () => {
-  return sass('scss/**/index.scss', { style: 'expanded' }).on('error', sass.logError)
+  return gulp.src('scss/**/*.scss')
+    .pipe(sass({ style: 'expanded' }).on('error', sass.logError))
     .pipe(postcss([ autoprefixer ]))
-    .pipe(gulp.dest('css/'))
+    .pipe(gulp.dest('css'))
 })
 
 gulp.task('jade', () => {
